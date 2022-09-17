6c36d6cc363e8bb8fa750b478b021ca540db192d
Akash Yadav <akash2@gmail.com>
2022-09-17 17:42:06 +0530

    Make the login backend

--- GroupChat/backEnd/controller/user.js
+++ GroupChat/backEnd/controller/user.js
@@ -20,3 +20,27 @@ exports.postSign =async (req, res)=>{
        res.json({flag: false})
    } 
  }
+ exports.postlogin =async (req,res)=>{
+    let email = req.body.email
+    let password = req.body.password// user  through password
+    let userData =await user.findAll({where:{email:email}})
+
+     
+       if(userData.length>0){// user found
+          const userId = userData[0].id
+          const userName = userData[0].name
+          const userEmail = userData[0].email
+          const userPassHash = userData[0].password 
+
+          const match = await bycrpt.compare(password,userPassHash)
+          if(match){
+            let token = JWT.sign(userId,process.env.TOKEN_SECRET)
+             
+             res.status(202).json({msg:"Login successfull" , token:token, name: userName ,email:userEmail })
+          }else{
+            res.status(401).json({msg:"Login not authorised"})
+          }
+       }else{
+          res.status(404).json({msg:"user not found"})
+       }
+ }
